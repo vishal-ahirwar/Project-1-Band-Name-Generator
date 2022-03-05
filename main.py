4784f49b468d8f23cb170d263f9bc842566ccaf6
IVishalAhirwar <vishalchoudhry0091@gmail.com>
2022-03-05 21:08:25 +0530

    randomines implemented

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from random import Random, randrange
+
 print("WELCOME TO BAND NAME GENERATOR[FIRST PYTHON PROJECT]")
-name=input("What's Your Name : \n")
+question=["What's Your Name :","What's your city name : ","Your favorate fruit's name : "]
+name=input(question[randrange(0,len(question)-1)])
 #geting first name of user
 
 pet_name =input("What's Your Pet name : \n")
@@ -7,3 +10,4 @@ pet_name =input("What's Your Pet name : \n")
 
 print(f"Your band name should be {name} {pet_name}")
 #generate band name combining username and user's pet name
+
