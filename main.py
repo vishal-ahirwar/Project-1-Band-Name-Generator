286c2059899339e97674e099fbe27ee52454fca6
IVishalAhirwar <vishalchoudhry0091@gmail.com>
2022-03-05 20:55:09 +0530

    python i'm comming

---
+++ main.py
@@ -0,0 +1,4 @@
+print("WELCOME TO BAND NAME GENERATOR[FIRST PYTHON PROJECT]")
+name=input("What's Your Name : \n")
+pet_name =input("What's Your Pet name : \n")
+print(f"Your band name should be {name} {pet_name}")
